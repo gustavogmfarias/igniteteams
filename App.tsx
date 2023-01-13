3baf12d72a71ea9fdeaa766b76786fa8f85f4f4e
Gustavo <gustavogmfarias@gmail.com>
2023-01-13 15:57:31 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
+import { ThemeProvider } from "styled-components";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import theme from "./src/theme";
 import { Groups } from "./src/screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups /> : }
+    </ThemeProvider>
+  );
 }
