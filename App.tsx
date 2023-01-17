ac6f0d07f99ba6c935e42c5690fe4d88c24c6e2b
Gustavo <gustavogmfarias@gmail.com>
2023-01-17 15:16:28 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 import {
@@ -8,13 +9,19 @@ import {
 
 import theme from "./src/theme";
 import { Groups } from "./src/screens/Groups";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : }
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container, LoadIndicator } from "./style";
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIndicator />
+    </Container>
+  );
+}
