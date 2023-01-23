b3005d6cc3863a5211503a19450e0ba8025e1673
Gustavo <gustavogmfarias@gmail.com>
2023-01-23 11:27:04 -0300

    .

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,23 @@
+import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { useState } from "react";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
   return (
     <Container>
       <Header />
       <Highlight title="Turma" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
