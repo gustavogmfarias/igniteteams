bf9733f454b9feffc836dc4846c65f5281092897
Gustavo <gustavogmfarias@gmail.com>
2023-01-24 11:05:42 -0300

    .

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,9 +5,11 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { useState } from "react";
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -17,7 +19,13 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
