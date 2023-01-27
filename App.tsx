fdbb8e487ca2de7c8154daa3d07a4ef7e5d361a8
Gustavo <gustavogmfarias@gmail.com>
2023-01-27 15:12:23 -0300

    .

--- App.tsx
+++ App.tsx
@@ -9,19 +9,22 @@ import {
 import theme from "./src/theme";
 
 import { Loading } from "@components/Loading";
-import { Players } from "@screens/Players";
+
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Players /> : <Loading />}
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </>
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,21 @@
-import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
 import { FlatList } from "react-native";
 
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { useState } from "react";
-import { Container } from "./styles";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
+import { Container } from "./styles";
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
-      <Highlight title="Turma" subtitle="Jogue com a sua turma" />
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
 
       <FlatList
         data={groups}
@@ -23,10 +25,9 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
-        showsVerticalScrollIndicator={false}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={() => {}} />
     </Container>
   );
 }
